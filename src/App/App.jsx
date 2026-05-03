782f261b3cedc8ae0ef429f49483aa2d25eac175
vanhog <dieter@hoogestraat.dk>
2026-05-03 19:17:46 +0200

    Initial commit

---
+++ src/App/App.jsx
@@ -0,0 +1,8 @@
+import MakeCard from '../MakeCard/MakeCard';
+import { articles } from './articles.js';
+
+export default function App() {
+  const mydata = articles;
+
+  return mydata.map((elem) => <MakeCard key={elem.id} article={elem} />);
+}
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App/App';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <div className="flex flex-row flex-wrap justify-center items-start gap-2 bg-gray-200">
+      <App />
+    </div>
+  </StrictMode>,
+);
