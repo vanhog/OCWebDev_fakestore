8b731822bea4a5623b0f3ad7b0bcc1d69bbf42b3
vanhog <dieter@hoogestraat.dk>
2026-05-03 19:33:21 +0200

    Clean up for shared use

--- src/App/App.jsx
+++
@@ -1,8 +0,0 @@
-import MakeCard from '../MakeCard/MakeCard';
-import { articles } from './articles.js';
-
-export default function App() {
-  const mydata = articles;
-
-  return mydata.map((elem) => <MakeCard key={elem.id} article={elem} />);
-}
