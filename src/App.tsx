7a99ec2457126012cfda388599e0f75441a2d51c
vanhog <dieter@hoogestraat.dk>
2026-05-12 12:49:44 +0200

    Changed article layout from grid to flex-box

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 import { products } from './product.tsx';
 import MakeCard from './Components/MakeCard/MakeCard.tsx';
 
 export default function App() {
-  const [search, setSearch] = useState("");
-  const [sort, setSort] = useState("none");
+  const [search, setSearch] = useState('');
+  const [sort, setSort] = useState('none');
 
   const filtered = products
     .filter((p) => p.title.toLowerCase().includes(search.toLowerCase()))
     .sort((a, b) => {
-      if (sort === "price-asc") return a.price - b.price;
-      if (sort === "price-desc") return b.price - a.price;
+      if (sort === 'price-asc') return a.price - b.price;
+      if (sort === 'price-desc') return b.price - a.price;
       return 0;
     });
 
@@ -42,50 +42,29 @@ export default function App() {
         </select>
 
         <div className="flex gap-2 flex-wrap justify-center">
-          {["electronics", "jewelery", "men's clothing", "women's clothing"].map(
-            (cat) => (
-              <button
-                key={cat}
-                className="bg-white text-orange-500 px-3 py-1 rounded-full text-sm"
-              >
-                {cat}
-              </button>
-            )
-          )}
+          {[
+            'electronics',
+            'jewelery',
+            "men's clothing",
+            "women's clothing",
+          ].map((cat) => (
+            <button
+              key={cat}
+              className="bg-white text-orange-500 px-3 py-1 rounded-full text-sm"
+            >
+              {cat}
+            </button>
+          ))}
         </div>
       </div>
 
       {/* Products  */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+
+      <div className="flex min-h-screen flex-row flex-wrap items-start justify-center gap-3 bg-gray-200 p-6">
         {filtered.map((product) => (
-                <MakeCard key={product.id} article={product} />
-          // <div
-          //   key={product.id}
-          //   className="bg-white rounded-xl shadow-md p-4 flex flex-col"
-          // >
-          //   <img
-          //     src={product.image}
-          //     alt={product.title}
-          //     className="h-60 object-contain mb-4"
-          //   />
-
-          //   <h2 className="text-gray-700 font-semibold text-sm mb-2">
-          //     {product.title}
-          //   </h2>
-
-          //   <div className="mt-auto flex items-center justify-between">
-          //     <span className="text-green-600 font-bold">
-          //       $ {product.price}
-          //     </span>
-          //     <button className="bg-orange-500 text-white px-3 py-1 rounded-md">
-          //       Add to cart
-          //     </button>
-          //   </div>
-          // </div>
-        )
-        )}
+          <MakeCard key={product.id} article={product} />
+        ))}
       </div>
     </div>
   );
 }
-
