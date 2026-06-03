e6c83d7dc64513f15de4d4a506551410b6ae08ba
vanhog <dieter@hoogestraat.dk>
2026-06-03 17:45:21 +0200

    Add cart button

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,20 +1,18 @@
-import { Link, useNavigate } from "react-router-dom";
-
+import { Link, useNavigate } from 'react-router-dom';
 
 function Navbar() {
   const navigate = useNavigate();
 
-  const user = localStorage.getItem("user");
+  const user = localStorage.getItem('user');
 
   const logout = () => {
-    localStorage.removeItem("user");
+    localStorage.removeItem('user');
 
-    navigate("/");
+    navigate('/');
   };
 
   return (
-    <nav className="flex justify-end items-center p-4 border-b">
-
+    <nav className="flex justify-end items-center p-4 border-b gap-3">
       {user ? (
         <button
           onClick={logout}
@@ -23,13 +21,18 @@ function Navbar() {
           Logout
         </button>
       ) : (
-        <Link
-          to="/login"
-          className="bg-black text-white px-4 py-2 rounded"
-        >
+        <Link to="/login" className="bg-black text-white px-4 py-2 rounded">
           Login
         </Link>
       )}
+      <div className="w-8 h-8 bg-no-repeat bg-center bg-contain bg-[url('/src/Components/cart.svg')]">
+        <div
+          id="noItems"
+          className="flex justify-center items-center bg-rose-300 w-4 h-4 rounded-[50%]"
+        >
+          0
+        </div>
+      </div>
     </nav>
   );
 }
